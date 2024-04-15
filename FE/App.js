b2ab22fa13449364b823f8d4330c69e3e0d6525d
3dlalswl3 <3dlalswl3@naver.com>
2024-04-15 16:59:44 +0900

    feat: react native 초기 프로젝트 생성

--- FE/App.js
+++ FE/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>안녕</Text>
+      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
