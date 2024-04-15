8a5426a53128fb9b2ca2052fbdd1ed78f94bd915
이현진 <hgene0929@gmail.com>
2024-04-15 10:37:07 +0900

    Feat: 애플리케이션 초기세팅

---
+++ BE/steptodance/src/main/java/com/dance101/steptodance/SteptodanceApplication.java
@@ -0,0 +1,13 @@
+package com.dance101.steptodance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SteptodanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SteptodanceApplication.class, args);
+	}
+
+}
