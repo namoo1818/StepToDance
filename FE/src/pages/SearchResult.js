b9d9b082498298f485ee5df2f2cec8e71c39cf65
3dlalswl3 <3dlalswl3@naver.com>
2024-04-16 17:40:35 +0900

    study: 메인 페이지 구현

---
+++ FE/src/pages/SearchResult.js
@@ -0,0 +1,20 @@
+import React from "react";
+import {View, Text, Button, ScrollView} from "react-native";
+import VideoList from "../components/video/VideoList";
+
+export default function SearchResult(){
+    return (
+        <View style={styles.root}>
+            <SearchBar/>
+            <ScrollView>
+                <VideoList/>
+            </ScrollView>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    root: {
+      backgroundColor:'black',
+    },
+  });
