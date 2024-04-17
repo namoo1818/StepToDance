49bb4451ee9cdd51a646e2434a8193a425b10190
이현진 <hgene0929@gmail.com>
2024-04-17 13:01:22 +0900

    Feat: 피드백 삭제 API 구현

--- BE/steptodance/src/main/java/com/dance101/steptodance/feedback/controller/FeedbackController.java
+++ BE/steptodance/src/main/java/com/dance101/steptodance/feedback/controller/FeedbackController.java
@@ -3,13 +3,16 @@ package com.dance101.steptodance.feedback.controller;
 import com.dance101.steptodance.feedback.data.response.FeedbackFindResponse;
 import com.dance101.steptodance.feedback.service.FeedbackService;
 import com.dance101.steptodance.global.data.response.ApiResponse;
-import com.dance101.steptodance.global.data.response.StatusCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import static com.dance101.steptodance.global.data.response.StatusCode.SUCCESS_FEEDBACK_DELETION;
+import static com.dance101.steptodance.global.data.response.StatusCode.SUCCESS_FEEDBACK_ONE;
+import static org.springframework.http.HttpStatus.NO_CONTENT;
 import static org.springframework.http.HttpStatus.OK;
 
 @RequiredArgsConstructor
@@ -20,6 +23,12 @@ public class FeedbackController {
     @GetMapping("/{feedback_id}")
     public ResponseEntity<ApiResponse<FeedbackFindResponse>> findFeedback(@PathVariable("feedback_id") long feedbackId) {
         FeedbackFindResponse response = feedbackService.findFeedback(feedbackId);
-        return ApiResponse.toResponse(OK, StatusCode.SUCCESS_FEEDBACK_ONE, response);
+        return ApiResponse.toResponse(OK, SUCCESS_FEEDBACK_ONE, response);
+    }
+
+    @DeleteMapping("/{feedback_id}")
+    public ResponseEntity<ApiResponse<Void>> deleteFeedback(@PathVariable("feedback_id") long feedbackId) {
+        //
+        return ApiResponse.toEmptyResponse(NO_CONTENT, SUCCESS_FEEDBACK_DELETION);
     }
 }
