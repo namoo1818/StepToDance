d8166b54f9c128350452cd22bce27f4bbdb5133c
이현진 <hgene0929@gmail.com>
2024-04-18 11:03:37 +0900

    Feat: 가이드에 대한 피드백 생성 API 구현

--- BE/steptodance/src/main/java/com/dance101/steptodance/guide/service/GuideService.java
+++ BE/steptodance/src/main/java/com/dance101/steptodance/guide/service/GuideService.java
@@ -1,11 +1,17 @@
 package com.dance101.steptodance.guide.service;
 
+import com.dance101.steptodance.feedback.data.response.FeedbackFindResponse;
+import com.dance101.steptodance.guide.data.request.GuideFeedbackCreateRequest;
 import com.dance101.steptodance.guide.data.request.SearchConditions;
 import com.dance101.steptodance.guide.data.response.GuideFindResponse;
 import com.dance101.steptodance.guide.data.response.GuideListFindResponse;
+
+import java.util.concurrent.CompletableFuture;
 
 public interface GuideService {
 	GuideListFindResponse findGuideList(SearchConditions searchConditions);
 
 	GuideFindResponse findGuide(long guideId);
+
+	CompletableFuture<FeedbackFindResponse> createGuideFeedback(long userId, long guideId, GuideFeedbackCreateRequest guideFeedbackCreateRequest);
 }
--- BE/steptodance/src/main/java/com/dance101/steptodance/guide/service/GuideServiceImpl.java
+++ BE/steptodance/src/main/java/com/dance101/steptodance/guide/service/GuideServiceImpl.java
@@ -1,15 +1,19 @@
 package com.dance101.steptodance.guide.service;
 
+import com.dance101.steptodance.feedback.data.response.FeedbackFindResponse;
 import com.dance101.steptodance.global.exception.category.NotFoundException;
+import com.dance101.steptodance.guide.data.request.GuideFeedbackCreateRequest;
 import com.dance101.steptodance.guide.data.request.SearchConditions;
 import com.dance101.steptodance.guide.data.response.GuideFindResponse;
 import com.dance101.steptodance.guide.data.response.GuideListFindResponse;
 import com.dance101.steptodance.guide.repository.GuideRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 
 import static com.dance101.steptodance.global.exception.data.response.ErrorCode.GUIDE_NOT_FOUND;
 
@@ -34,5 +38,24 @@ public class GuideServiceImpl implements GuideService{
 		// get guide response & return
 		return guideRepository.findGuideByGuideId(guideId)
 			.orElseThrow(() -> new NotFoundException("GuideServiceImpl:findGuide", GUIDE_NOT_FOUND));
+	}
+
+	@Async
+	@Transactional
+	@Override
+	public CompletableFuture<FeedbackFindResponse> createGuideFeedback(long userId, long guideId, GuideFeedbackCreateRequest guideFeedbackCreateRequest) {
+		try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+			Thread.currentThread().interrupt();
+		}
+
+		System.out.println("============================================");
+		System.out.println(guideFeedbackCreateRequest.startAt());
+		System.out.println(guideFeedbackCreateRequest.endAt());
+		System.out.println(guideFeedbackCreateRequest.videoUrl());
+		System.out.println("============================================");
+
+		return CompletableFuture.completedFuture(new FeedbackFindResponse(null, null));
 	}
 }
