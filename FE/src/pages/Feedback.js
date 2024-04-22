4c007d1d7237a1a7b531295e8fc5c100457c07be
3dlalswl3 <3dlalswl3@naver.com>
2024-04-22 10:13:34 +0900

    피드백 페이지 수정

--- FE/src/pages/Feedback.js
+++ FE/src/pages/Feedback.js
@@ -1,24 +1,9 @@
-import React, { useEffect, useState, useCallback, useRef } from "react";
+import React, { useEffect, useState } from "react";
 import { View, Text, Image, ScrollView, StyleSheet, Button } from "react-native";
 import { LinearGradient } from 'expo-linear-gradient'
-import Video, {VideoRef} from 'react-native-video'
-import YoutubePlayer from "react-native-youtube-iframe";
 
 function Feedback({ navigation, route }) {
   const guide = route.params;
-  const videoRef = useRef<VideoRef>(null);
-
-  const [playing, setPlaying] = useState(false);
-
-  const onStateChange = useCallback((state) => {
-    if (state === "ended") {
-      setPlaying(false);
-    }
-  }, []);
-
-  const togglePlaying = useCallback(() => {
-    setPlaying((prev) => !prev);
-  }, []);
 
   useEffect(() => {
     navigation.setOptions({
@@ -30,25 +15,7 @@ function Feedback({ navigation, route }) {
       <Button title="뒤로가기" onPress={()=>navigation.goBack()}/>
       <View style={styles.container}>
         <Text style={styles.text}>피드백 화면</Text>
-        <View>
-          <YoutubePlayer
-          height={250}
-          width={400}
-          play={playing}
-          videoId={"hP-ijKLYLR8"}
-          onChangeState={onStateChange}
-          />
-          <YoutubePlayer
-          height={250}
-          width={400}
-          play={playing}
-          videoId={"MiNvonMuK94"}
-          onChangeState={onStateChange}
-          />
-          <Button title={playing ? "pause" : "play"} onPress={togglePlaying} />
-        </View>
         <Text style={styles.text}>SCORE</Text>
-        <Text style={styles.text}>90</Text>
       </View>
     </LinearGradient>
   );
