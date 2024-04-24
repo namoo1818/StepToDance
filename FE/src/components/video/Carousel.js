7960c1f92397535bf06447762629df3d3a288b6a
3dlalswl3 <3dlalswl3@naver.com>
2024-04-24 16:09:19 +0900

    feat: 캐러셀 수정

--- FE/src/components/video/Carousel.js
+++ FE/src/components/video/Carousel.js
@@ -1,3 +1,69 @@
 import React, { useState } from 'react';
-import { FlatList } from 'react-native';
-import styled from 'styled-components/native';
+import { FlatList, View, StyleSheet } from 'react-native';
+import Page from './Page';
+
+export default function Carousel({ pages, pageWidth, gap, offset }) {
+  const [page, setPage] = useState(0);
+
+  function renderItem({ item }) {
+    return (
+      <Page item={item} style={{ width: pageWidth, marginHorizontal: gap / 2 }} />
+    );
+  }
+
+  const onScroll = (e) => {
+    const newPage = Math.round(
+      e.nativeEvent.contentOffset.x / (pageWidth + gap),
+    );
+    setPage(newPage);
+  };
+
+  return (
+    <View style={styles.container}>
+      <FlatList
+        automaticallyAdjustContentInsets={false}
+        contentContainerStyle={{
+          paddingHorizontal: offset + gap / 2,
+        }}
+        data={pages}
+        decelerationRate="fast"
+        horizontal
+        keyExtractor={(item) => `page__${item.color}`}
+        onScroll={onScroll}
+        pagingEnabled
+        renderItem={renderItem}
+        snapToInterval={pageWidth + gap}
+        snapToAlignment="start"
+        showsHorizontalScrollIndicator={false}
+      />
+      <View style={styles.indicatorWrapper}>
+        {Array.from({ length: pages.length }, (_, i) => i).map((i) => (
+          <View key={`indicator_${i}`} style={[styles.indicator, i === page && styles.focused]} />
+        ))}
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    height: '60%',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  indicator: {
+    margin: 4,
+    backgroundColor: '#dfdfdf',
+    width: 6,
+    height: 6,
+    borderRadius: 3,
+  },
+  focused: {
+    backgroundColor: '#262626',
+  },
+  indicatorWrapper: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginTop: 16,
+  },
+});
--- FE/src/components/video/Page.js
+++ FE/src/components/video/Page.js
@@ -1,7 +1,19 @@
 import React from 'react';
-import styled from 'styled-components/native';
-import {ViewStyle} from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 
-export default function Page(){
-    
+export default function Page({ item, style }) {
+  return (
+    <View style={[styles.pageItem, { backgroundColor: item.color }, style]}>
+      <Text style={styles.pageNum}>{item.num}</Text>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  pageItem: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 20,
+  },
+  pageNum: {},
+});
