51c263b4f52bb1ad230c781d2b5eca98046a6843
3dlalswl3 <3dlalswl3@naver.com>
2024-04-24 13:30:26 +0900

    feat: 가이드영상목록 조회 api 연동

--- FE/src/components/video/VideoList.js
+++ FE/src/components/video/VideoList.js
@@ -1,101 +1,38 @@
-import React from "react";
-import { View, Text, Image, StyleSheet, TouchableOpacity, FlatList } from "react-native";
+import React, { useEffect, useState } from "react";
+import { View, Text, FlatList, TouchableOpacity } from "react-native";
 import { useNavigation } from "@react-navigation/native";
-import GudieApiRequest from "../../api/gudie";
-
-const g = GudieApiRequest.getGuideList();
-
-const guideList = [
-  {
-    "id": 1,
-    "video_url": "https://example.com/guide_video_1",
-    "thumbnail_img_url": "../../assets/thumbnail.png",
-    "song_title": "Yesterday",
-    "singer": "The Beatles",
-    "genre": "Rock",
-    "rank": 1,
-    "uploader": "user123",
-    "count_feedback": 5,
-    "created_at": "2024-04-15T08:00:00Z"
-  },
-  {
-    "id": 2,
-    "video_url": "https://example.com/guide_video_2",
-    "thumbnail_img_url": "../../assets/thumbnail.png",
-    "song_title": "Shape of You",
-    "singer": "Ed Sheeran",
-    "genre": "Pop",
-    "rank": 2,
-    "uploader": "user456",
-    "count_feedback": 3,
-    "created_at": "2024-04-14T10:30:00Z"
-  },
-  {
-    "id": 3,
-    "video_url": "https://example.com/guide_video_3",
-    "thumbnail_img_url": "../../assets/thumbnail.png",
-    "song_title": "Bohemian Rhapsody",
-    "singer": "Queen",
-    "genre": "Rock",
-    "rank": 3,
-    "uploader": "user789",
-    "count_feedback": 7,
-    "created_at": "2024-04-13T15:45:00Z"
-  },
-  {
-    "id": 4,
-    "video_url": "https://example.com/guide_video_3",
-    "thumbnail_img_url": "../../assets/thumbnail.png",
-    "song_title": "Smoke",
-    "singer": "Dynamic Duo",
-    "genre": "HipHop",
-    "rank": 4,
-    "uploader": "user789",
-    "count_feedback": 7,
-    "created_at": "2024-04-13T15:45:00Z"
-  },
-];
+import { getGuideList } from "../../api/GuideApis";
 
 export default function VideoList() {
+  const [guideList, setGuideList] = useState([]);
   const navigation = useNavigation();
 
+  useEffect(() => {
+    const fetchGuideData = async () => {
+      try {
+        const data = await getGuideList();
+        setGuideList(data);
+      } catch (error) {
+        console.error('Error fetching guide data:', error);
+      }
+    };
+
+    fetchGuideData();
+  }, []);
+
   const renderItem = ({ item }) => (
-    <TouchableOpacity style={styles.guide} onPress={() => navigation.navigate("GuideDetail", item)}>
-      <Image style={styles.image} source={require("../../assets/thumbnail.png")} />
-      <Text style={styles.text}>{item.song_title} - {item.singer}</Text>
+    <TouchableOpacity onPress={() => navigation.navigate("GuideDetail", { guideId: item.id })}>
+      <Text>{item.title}</Text>
     </TouchableOpacity>
   );
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>민지님이 좋아하는 케이팝 안무</Text>
+    <View>
       <FlatList
         data={guideList}
         renderItem={renderItem}
-        keyExtractor={item => item.id.toString()}
+        keyExtractor={(item) => item.id.toString()}
       />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  text: {
-    color: 'white',
-    fontSize: 20,
-    margin: 10,
-    textAlignVertical: 'center',
-  },
-  image: {
-    width: 100,
-    height: 100,
-    borderRadius: 5,
-  },
-  guide: {
-    flexDirection: 'row',
-    marginTop: 10,
-    marginBottom: 10,
-  },
-});
