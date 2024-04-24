9739824d87a2410622f1af590166b06783473827
황진하 <hjh70902000@naver.com>
2024-04-24 13:22:48 +0900

    Fix: 카프카 테스트

--- AI/src/config.py
+++ AI/src/config.py
@@ -1,7 +1,7 @@
 import asyncio
 
 # env Variable
-KAFKA_BOOTSTRAP_SERVERS="localhost:9092"
+KAFKA_BOOTSTRAP_SERVERS="k10a101.p.ssafy.io:9092"
 KAFKA_TOPIC="topic-ai"
 KAFKA_CONSUMER_GROUP="group.id"
 loop = asyncio.get_event_loop()
