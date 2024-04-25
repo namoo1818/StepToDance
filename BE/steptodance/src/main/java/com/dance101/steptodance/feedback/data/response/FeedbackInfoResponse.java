6775dbdec2ba076ff1a390cec11600ee0bce4085
이현진 <hgene0929@gmail.com>
2024-04-25 16:28:51 +0900

    Fix: 피드백 영상 상세조회 API 오류 수정

--- BE/steptodance/src/main/java/com/dance101/steptodance/feedback/data/response/FeedbackInfoResponse.java
+++ BE/steptodance/src/main/java/com/dance101/steptodance/feedback/data/response/FeedbackInfoResponse.java
@@ -2,12 +2,14 @@ package com.dance101.steptodance.feedback.data.response;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import java.time.LocalTime;
+
 public record FeedbackInfoResponse(
     @JsonProperty("id") long id,
     @JsonProperty("score") double score,
     @JsonProperty("video_url") String url,
     @JsonProperty("guide_url") String guideUrl,
-    @JsonProperty("highlight_section_start_at") String highlightSectionStartAt,
-    @JsonProperty("highlight_section_end_at") String highlightSectionEndAt
+    @JsonProperty("highlight_section_start_at") LocalTime highlightSectionStartAt,
+    @JsonProperty("highlight_section_end_at") LocalTime highlightSectionEndAt
 ) {
 }
