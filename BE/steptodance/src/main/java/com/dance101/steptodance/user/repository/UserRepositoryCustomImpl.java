68e1d8ef94c80212b11844d2c75d6dc2c04fe66e
이현진 <hgene0929@gmail.com>
2024-04-25 15:02:30 +0900

    Chore: 마이페이지 API 수정

--- BE/steptodance/src/main/java/com/dance101/steptodance/user/repository/UserRepositoryCustomImpl.java
+++ BE/steptodance/src/main/java/com/dance101/steptodance/user/repository/UserRepositoryCustomImpl.java
@@ -2,15 +2,11 @@ package com.dance101.steptodance.user.repository;
 
 import com.dance101.steptodance.global.utils.QueryUtils;
 import com.dance101.steptodance.user.data.response.TopRankerListResponse;
-import com.dance101.steptodance.user.data.response.UserFindResponse;
-import com.querydsl.core.types.ExpressionUtils;
 import com.querydsl.core.types.Projections;
-import com.querydsl.jpa.JPAExpressions;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 
 import java.util.List;
-import java.util.Optional;
 
 import static com.dance101.steptodance.feedback.domain.QFeedback.feedback;
 import static com.dance101.steptodance.user.domain.QUser.user;
@@ -19,26 +15,6 @@ import static com.dance101.steptodance.user.domain.QUser.user;
 public class UserRepositoryCustomImpl implements UserRepositoryCustom {
     private final JPAQueryFactory queryFactory;
     private final QueryUtils queryUtils;
-
-//    @Override
-//    public Optional<UserFindResponse> findUserByUserId(long userId) {
-//        return Optional.ofNullable(
-//            queryFactory.select(Projections.constructor(UserFindResponse.class,
-//                    user.profileImgUrl,
-//                    user.nickname,
-//                    ExpressionUtils.as(
-//                        JPAExpressions.select(queryUtils.createRankingSQL(feedback.score.sum()))
-//                            .from(feedback)
-//                            .groupBy(user.id)
-//                            .having(user.id.eq(userId)),
-//                        "userRank"
-//                    )
-//                ))
-//                .from(user)
-//                .where(user.id.eq(userId))
-//                .fetchOne()
-//        );
-//    }
 
     @Override
     public List<TopRankerListResponse> findTopRankerList() {
