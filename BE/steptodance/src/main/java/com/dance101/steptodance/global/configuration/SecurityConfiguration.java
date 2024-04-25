d740bf06ec1b9bdc018de13c5da45ccc41f017a9
이현진 <hgene0929@gmail.com>
2024-04-25 17:19:40 +0900

    Fix: 시큐리티 필터 제외

--- BE/steptodance/src/main/java/com/dance101/steptodance/global/configuration/SecurityConfiguration.java
+++ BE/steptodance/src/main/java/com/dance101/steptodance/global/configuration/SecurityConfiguration.java
@@ -35,7 +35,7 @@ public class SecurityConfiguration {
             .cors(cors -> cors.configurationSource(corsConfigurationSource()))
             .sessionManagement(configurer -> configurer.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
             .authorizeHttpRequests(authorize -> {
-                authorize.requestMatchers("/auth/login/**", "/guides").permitAll();
+                authorize.requestMatchers("/auth/login/**").permitAll();
                 authorize.anyRequest().authenticated();
             })
             .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class);
