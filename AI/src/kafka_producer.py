4294457b0eb9172491871a1f2d678bd968bd4010
황진하 <hjh70902000@naver.com>
2024-04-26 10:51:05 +0900

    Fix: spring과 같은 network 묶기

--- AI/src/kafka_producer.py
+++ AI/src/kafka_producer.py
@@ -2,7 +2,8 @@ import json
 from kafka import KafkaProducer
 
 producer = KafkaProducer(
-    bootstrap_servers='k10a101.p.ssafy.io:9092',
+    # bootstrap_servers='k10a101.p.ssafy.io:9092',
+    bootstrap_servers='kafka1:9092, kafka2:9092, kafka3:9092',
     value_serializer=lambda v: json.dumps(v).encode('utf-8')
 )
 
