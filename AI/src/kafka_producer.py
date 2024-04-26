46523cc46b6c271e9f5b6c954b493952ae1a296f
황진하 <hjh70902000@naver.com>
2024-04-26 16:46:36 +0900

    Fix: 파이썬 Kafka 모듈 획일화

--- AI/src/kafka_producer.py
+++ AI/src/kafka_producer.py
@@ -1,13 +1,16 @@
 import json
-from kafka import KafkaProducer
+from confluent_kafka import Producer
 
-producer = KafkaProducer(
-    # bootstrap_servers='k10a101.p.ssafy.io:9092',
-    bootstrap_servers='kafka1:9092, kafka2:9092, kafka3:9092',
-    value_serializer=lambda v: json.dumps(v).encode('utf-8')
-)
+# producer = KafkaProducer(
+#     bootstrap_servers='k10a101.p.ssafy.io:9092',
+#     bootstrap_servers='kafka1:9092, kafka2:9092, kafka3:9092',
+#     value_serializer=lambda v: json.dumps(v).encode('utf-8')
+# )
+producer_c = Producer({
+    'bootstrap.servers': 'kafka1:9092, kafka2:9092, kafka3:9093',
+})
 
 async def send_data_to_kafka(data: dict):
-    producer.send('topic-ai', value=data)
-    producer.flush()
+    producer_c.produce(topic='topic-ai', value=json.dump(data))
+    producer_c.flush()
     print(f'sent: {data}')
