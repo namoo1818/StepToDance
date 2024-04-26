b451ab5ccc952280b34351fddb868bf4bd563349
황진하 <hjh70902000@naver.com>
2024-04-26 10:28:32 +0900

    Fix: kafka bootstrap 변경

--- AI/src/config.py
+++ AI/src/config.py
@@ -2,7 +2,7 @@ import asyncio
 
 # env Variable
 # KAFKA_BOOTSTRAP_SERVERS="kafka1:9092"
-KAFKA_BOOTSTRAP_SERVERS="kafka1:9092, kafka2:9092, kafka3:9092"
+KAFKA_BOOTSTRAP_SERVERS="localhost:9092"
 KAFKA_TOPIC="topic-ai"
 KAFKA_CONSUMER_GROUP="group.id"
 # loop = asyncio.get_event_loop()
