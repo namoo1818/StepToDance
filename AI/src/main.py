2b60e5bac7580bdf414477e43a114fb7149b9085
황진하 <hjh70902000@naver.com>
2024-04-29 15:30:01 +0900

    Fix: ai서버에서 body를 모델로 받도록 변경

--- AI/src/main.py
+++ AI/src/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, BackgroundTasks
 from kafka_producer import send_data_to_kafka
 from confluent_kafka import Consumer
 import asyncio
+from data.guideUpdateRequest import guideUpdateRequest
 
 
 # consumer = Consumer({'bootstrap.servers': 'k10a101.p.ssafy.io:9092', 'group.id': 'group.id'})
@@ -15,7 +16,7 @@ def home():
     return "hello!"
 
 @app.post('/guides/upload')
-def guideUpload(data: dict):
+def guideUpload(data: guideUpdateRequest):
     print(data)
     return "hello!"
 
