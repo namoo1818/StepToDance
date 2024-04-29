9916a58b98215375c96328aaa2669838725961ff
3dlalswl3 <3dlalswl3@naver.com>
2024-04-29 17:22:20 +0900

    feat: foot 추가

--- FE/src/App.jsx
+++ FE/src/App.jsx
@@ -1,12 +1,21 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import "./App.css";
-import { Routes, Route } from "react-router";
 import MainPage from "./pages/MainPage/MainPage";
-
+import Home from './pages/Home';
+import VideoList from './pages/VideoList';
 function App() {
+
   return (
-    <Routes>
-      <Route path="/" element={<MainPage />} />
-    </Routes>
+    <div className='App'>
+      <Router>
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+          <Route path='/home' element={<Home/>}/>
+          <Route path='/videoList' element={<VideoList/>}/>
+        </Routes>
+      </Router>
+    </div>
   );
 }
 
---
+++ FE/src/pages/GuideUpload.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function GuideUpload(){
+    return (
+        <div>가이드 영상 업로드</div>
+    );
+}
+
+export default GuideUpload;
