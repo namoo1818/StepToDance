9cc7a46c682dfd701e2908de89065f1a4d19192a
최근영 <wwaall2626@naver.com>
2024-04-29 18:50:40 +0900

    docs: 초기 세팅 및 비디오

--- FE/src/App.jsx
+++ FE/src/App.jsx
@@ -1,11 +1,12 @@
-import "./App.css";
 import { Routes, Route } from "react-router";
 import MainPage from "./pages/MainPage/MainPage";
+import { WebcamStreamCapture } from "./pages/RecodingPage/RecodingPage";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<MainPage />} />
+      <Route path="/record" element={<WebcamStreamCapture />} />
     </Routes>
   );
 }
