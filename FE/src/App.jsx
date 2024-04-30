2a83dfbe483a62490c7f4bcc3664e33daea61025
백지윤 <wendy6992@naver.com>
2024-04-30 14:34:34 +0900

    Feat: Add react-cookie

--- FE/src/App.jsx
+++ FE/src/App.jsx
@@ -1,17 +1,42 @@
-import { Routes, Route } from "react-router";
+import React from "react";
+import { Route, Routes, useNavigate } from 'react-router-dom';
+import { useSelector } from "react-redux";
 import MainPage from "./pages/MainPage/MainPage";
 import MyPage from "./pages/MyPage/MyPage";
 import LoginPage from "./pages/LoginPage/LoginPage";
-
+import KakaoLogin from "./pages/LoginPage/KakaoLogin";
+// import { WebcamStreamCapture } from "./pages/RecordingPage/RecordingPage";
 import { WebcamStreamCapture } from "./pages/RecodingPage/RecodingPage";
+import { userLoggedInSelector } from "./stores/UserSlice";
+import { getCookie } from './cookie';
+import { useEffect } from 'react';
 
 function App() {
+  const user = useSelector(userLoggedInSelector);
+  const cookie = getCookie('accessToken');
+  console.log(cookie)
+  console.log(user)
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (cookie && user) {
+      navigate('/');
+    }
+    if (!cookie) {
+      navigate('/login');
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [cookie, user]);
   return (
     <Routes>
-      <Route path="/" element={<MainPage />} />
-      <Route path="/mypage" element={<MyPage />} />
-      <Route path="/record" element={<WebcamStreamCapture />} />
-      <Route path="/login" element={<LoginPage />} />
+      {cookie && user && (
+        <Route path="/" element={<MainPage />}>
+          <Route path="/mypage" element={<MyPage />} />
+          <Route path="/record" element={<WebcamStreamCapture />} />
+        </Route>
+      )}
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/api/v1/auth/login" element={<KakaoLogin />} />
     </Routes>
   );
 }
