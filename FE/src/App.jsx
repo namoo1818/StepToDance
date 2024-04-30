dcfafc29fb0271bbb8c292b5809a8af521c7c9bc
최근영 <wwaall2626@naver.com>
2024-04-30 18:20:35 +0900

    fix: router 수정
    Former-commit-id: 1eea6cff79c4f6bd1a8cedf9585881162d6a03ed

--- FE/src/App.jsx
+++ FE/src/App.jsx
@@ -1,50 +1,48 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes, useNavigate } from 'react-router-dom';
+import { Route, Routes, useNavigate } from "react-router-dom";
 import "./App.css";
 import { useSelector } from "react-redux";
 import MainPage from "./pages/MainPage/MainPage";
-import Home from './pages/Home';
-import VideoList from './pages/VideoList';
-import GuideUpload from './pages/GuideUpload';
-import VideoEditor from './pages/VideoEditor';
+import Home from "./pages/Home";
+import VideoList from "./pages/VideoList";
+import GuideUpload from "./pages/GuideUpload";
+import VideoEditor from "./pages/VideoEditor";
 import MyPage from "./pages/MyPage/MyPage";
 import LoginPage from "./pages/LoginPage/LoginPage";
 import KakaoLogin from "./pages/LoginPage/KakaoLogin";
 import { WebcamStreamCapture } from "./pages/RecordingPage/RecordingPage";
-// import { WebcamStreamCapture } from "./pages/RecodingPage/RecodingPage";
 import { userLoggedInSelector } from "./stores/UserSlice";
-import { getCookie } from './cookie';
-import { useEffect } from 'react';
+import { getCookie } from "./cookie";
+import { useEffect } from "react";
 
 function App() {
   const user = useSelector(userLoggedInSelector);
-  const cookie = getCookie('accessToken');
-  console.log(cookie)
-  console.log(user)
+  const cookie = getCookie("accessToken");
+  console.log(cookie);
+  console.log(user);
   const navigate = useNavigate();
 
   useEffect(() => {
     if (cookie && user) {
-      navigate('/home');
+      navigate("/home");
     }
     if (!cookie) {
-      navigate('/login');
+      navigate("/login");
     }
   }, [cookie, user]);
 
   return (
-    <div className='App'>
-        <Routes>
-          <Route path="/" element={<MainPage />} />
-          <Route path='/home' element={<Home/>}/>
-          <Route path='/videoList' element={<VideoList/>}/>
-          <Route path='/guideUpload' element={<GuideUpload/>}/>
-          <Route path='/mypage' element={<MyPage/>}/>
-          <Route path='/videoEditor' element={<VideoEditor/>}/>
-          <Route path="/record" element={<WebcamStreamCapture />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/api/v1/auth/login" element={<KakaoLogin />} />
-        </Routes>
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/videoList" element={<VideoList />} />
+        <Route path="/guideUpload" element={<GuideUpload />} />
+        <Route path="/mypage" element={<MyPage />} />
+        <Route path="/videoEditor" element={<VideoEditor />} />
+        <Route path="/record" element={<WebcamStreamCapture />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/api/v1/auth/login" element={<KakaoLogin />} />
+      </Routes>
     </div>
   );
 }
