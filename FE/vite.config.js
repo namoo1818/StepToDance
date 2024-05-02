145ad44444669fe98dc4c31ed20c6039241e0b6f
3dlalswl3 <3dlalswl3@naver.com>
2024-05-02 11:14:06 +0900

    feat: 영상 편집
    Former-commit-id: 416cd90b213e7c17792c2798c5a8ad6f1bcf0fcc

--- FE/vite.config.js
+++ FE/vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import crossOriginIsolation from 'vite-plugin-cross-origin-isolation';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), crossOriginIsolation()],
   build: {
     rollupOptions: {
       input: {
