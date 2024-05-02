f6fcab5cba93437b10e3ec506e6daf950ecafdba
최근영 <wwaall2626@naver.com>
2024-05-02 20:52:03 +0900

    Feat: 랜딩 페이지 완성
    Former-commit-id: 4cbbe28a7c719be4172dbd7a542fcab1cc7f4cea

--- FE/src/pages/LandingPage/LandingPage.jsx
+++ FE/src/pages/LandingPage/LandingPage.jsx
@@ -1,21 +1,76 @@
+import { useEffect, useState } from "react";
 import styles from "./LandingPage.module.css";
 
 const LandingPage = () => {
+  const [isClicked, setIsClicked] = useState(false);
+  const [currentBox, setCurrentBox] = useState(1);
+
+  useEffect(() => {
+    let timeout;
+    if (isClicked) {
+      timeout = setTimeout(() => {
+        if (currentBox < 10) {
+          setCurrentBox((prev) => prev + 1);
+        }
+        const select = document.querySelector(`#box${currentBox}`);
+        select.style.display = "none";
+      }, 300);
+    }
+    return () => clearTimeout(timeout);
+  }, [isClicked, currentBox]);
+  const clickHandler = () => {
+    setIsClicked(!isClicked);
+  };
   return (
-    <section className={styles["landing-page"]}>
+    <section
+      className={
+        isClicked ? styles["landing-page-click"] : styles["landing-page"]
+      }
+      onClick={clickHandler}
+    >
       <div className={styles.first}>
-        <div className={styles["first-box"]}></div>
-        <div className={styles["second-box"]}></div>
-        <div className={styles["third-box"]}></div>
-        <div className={styles["fourth-box"]}></div>
-        <div className={styles["fifth-box"]}></div>
-        <div className={styles["sixth-box"]}></div>
-        <div className={styles["seventh-box"]}></div>
-        <div className={styles["eighth-box"]}></div>
-        <div className={styles["ninth-box"]}></div>
-        <div className={styles["tenth-box"]}></div>
+        <div
+          id="box1"
+          className={isClicked ? styles["box1"] : styles["first-box"]}
+        ></div>
+        <div
+          id="box2"
+          className={isClicked ? styles["box2"] : styles["second-box"]}
+        ></div>
+        <div
+          id="box3"
+          className={isClicked ? styles["box3"] : styles["third-box"]}
+        ></div>
+        <div
+          id="box4"
+          className={isClicked ? styles["box4"] : styles["fourth-box"]}
+        ></div>
+        <div
+          id="box5"
+          className={isClicked ? styles["box5"] : styles["fifth-box"]}
+        ></div>
+        <div
+          id="box6"
+          className={isClicked ? styles["box6"] : styles["sixth-box"]}
+        ></div>
+        <div
+          id="box7"
+          className={isClicked ? styles["box7"] : styles["seventh-box"]}
+        ></div>
+        <div
+          id="box8"
+          className={isClicked ? styles["box8"] : styles["eighth-box"]}
+        ></div>
+        <div
+          id="box9"
+          className={isClicked ? styles["box9"] : styles["ninth-box"]}
+        ></div>
+        <div
+          id="box10"
+          className={isClicked ? styles["box10"] : styles["tenth-box"]}
+        ></div>
       </div>
-      <span className={styles["intro"]}>Click Anywhere</span>
+      {!isClicked && <span className={styles["intro"]}>Click Anywhere</span>}
     </section>
   );
 };
