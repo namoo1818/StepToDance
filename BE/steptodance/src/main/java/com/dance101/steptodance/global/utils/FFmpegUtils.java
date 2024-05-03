241201b428cd7bb1591b9aa4836016aa17ecc424
Jinha Hwang <hjh70902000@gmail.com>
2024-05-03 11:24:46 +0900

    Fix: ffmpeg 문제 해결

--- BE/steptodance/src/main/java/com/dance101/steptodance/global/utils/FFmpegUtils.java
+++ BE/steptodance/src/main/java/com/dance101/steptodance/global/utils/FFmpegUtils.java
@@ -5,15 +5,10 @@ import java.io.IOException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-import net.bramp.ffmpeg.FFmpeg;
-import net.bramp.ffmpeg.FFprobe;
-
 @Component
 public class FFmpegUtils {
 	private final String ffmpegPath;
 	private final String ffprobePath;
-	private final FFmpeg ffmpeg;
-	private final FFprobe fFprobe;
 
 	public FFmpegUtils(
 		@Value("${ffmpeg.location}")String ffmpegPath,
@@ -21,7 +16,5 @@ public class FFmpegUtils {
 	) throws IOException {
 		this.ffmpegPath = ffmpegPath;
 		this.ffprobePath = ffprobePath;
-		ffmpeg = new FFmpeg(this.ffmpegPath);
-		fFprobe = new FFprobe(this.ffprobePath);
 	}
 }
