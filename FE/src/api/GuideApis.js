fe922f8f866391e24141855eeb41b8cfd127792d
3dlalswl3 <3dlalswl3@naver.com>
2024-05-03 14:25:05 +0900

    feat: 가이드영상 목록 리스트
    Former-commit-id: 4102dce7ffec4809cae9b541813defdb3d2b2593

---
+++ FE/src/api/GuideApis.js
@@ -0,0 +1,43 @@
+import axios from 'axios';
+import { getCookie } from '../cookie';
+const API_BASE_URL = 'https://k10a101.p.ssafy.io/api/v1';
+
+const accessToken = getCookie('accessToken');
+
+export const getGuideList = async () => {
+  try {
+    const response = await axios.get(`${API_BASE_URL}/guides?limit=10&offset=1`, {
+        headers: {
+            'Authorization': `Bearer ${accessToken}`,
+        },
+    });
+    return response.data;
+  } catch (error) {
+    console.error('Error fetching guide list:', error);
+    throw error; 
+  }
+};
+
+export const getGuideDetail = async (guideId) => {
+  try {
+    const response = await axios.get(`${API_BASE_URL}/guides/${guideId}`, {
+        headers: {
+            'Authorization': `Bearer ${accessToken}`,
+        },
+    });
+    return response.data;
+  } catch (error) {
+    console.error('Error fetching guide detail:', error);
+    throw error;
+  }
+};
+
+export const postFeedback = async (guideId, feedbackData) => {
+  try {
+    const response = await axios.post(`${API_BASE_URL}/guides/${guideId}/feedback`, feedbackData);
+    return response.data;
+  } catch (error) {
+    console.error('Error posting feedback:', error);
+    throw error;
+  }
+};
--- FE/src/components/guide/GuideList.jsx
+++ FE/src/components/guide/GuideList.jsx
@@ -1,10 +1,52 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import {useSelector, useDispatch} from 'react-redux';
 import { Link } from "react-router-dom";
+import styles from "../../styles/guide/GuideList.module.css";
+import { getGuideList } from "../../api/GuideApis";
+import styled from "styled-components";
 
 function GuideList(){
+    const user = useSelector(state=> state.user);
+    const [guideList, setGuideList] = useState([]);
+
+    useEffect(() => {
+        const fetchGuideData = async () => {
+          try {
+            const data = await getGuideList();
+            console.log(data.data.guide_list);
+            setGuideList(data.data.guide_list);
+          } catch (error) {
+            console.error('Error fetching guide data:', error);
+          }
+        };
+    
+        fetchGuideData();
+      }, []);
+    
+    const renderItem = ({item}) => (
+        <Link to={{ pathname: '/guideDetail', search: `?id=${item.id}` }}>
+            <div className={styles.guide}>
+                <img className={styles.image} src={item.thumbnail_img_url}/>
+                <p className={styles.text}>{item.song_title} - {item.singer}</p>
+            </div>
+        </Link>
+    );
     return (
-        <p>가이드 영상 목록</p>
+        <div className={styles.container}>
+            <div className={styles.title}>{user.nickname}님이 좋아하는 장르</div>
+            <Links>
+                {guideList.map((item) => (
+                <div key={`page_${item.id}`}>{renderItem({ item })}</div>
+                ))}
+            </Links>
+        </div>
     );
 }
+
+const Links = styled.div`
+    a {
+        text-decoration: none;
+    }
+`;
 
 export default GuideList;
