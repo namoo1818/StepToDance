0feeb8ef08da2af3b4dc0a599971cfa8394ced25
3dlalswl3 <3dlalswl3@naver.com>
2024-05-07 09:57:04 +0900

    feat: 검색 페이지
    Former-commit-id: 45dc56abccfdd474a203e831fb44b6f838bdb435

--- FE/src/pages/SearchResult.jsx
+++ FE/src/pages/SearchResult.jsx
@@ -1,5 +1,19 @@
+import { useLocation } from "react-router-dom";
+import SearchBar from "../components/guide/SearchBar";
+import SearchResultList from "../components/guide/SearchResultList";
+
 function SearchResult() {
-  return <div>검색 결과</div>;
+  const location = useLocation();
+  const searchParams = new URLSearchParams(location.search);
+  const searchQuery = searchParams.get("q");
+
+
+  return (
+    <div>
+      <SearchBar params={searchQuery}/>
+      <SearchResultList/>
+    </div>
+  );
 }
 
 export default SearchResult;
