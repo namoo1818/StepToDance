09ec9f3301bb0cc2813a9037652ad40b26e5fd46
3dlalswl3 <3dlalswl3@naver.com>
2024-05-07 14:25:48 +0900

    feat: 검색어 삭제 기능 추가
    Former-commit-id: 6b673b950f5509ceb9a13685d8d07fdd5d78bd87

--- FE/src/components/guide/SearchBar.jsx
+++ FE/src/components/guide/SearchBar.jsx
@@ -2,6 +2,7 @@ import {useState, useEffect} from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import styles from "../../styles/guide/SearchBar.module.css"
 import SearchIcon from "@mui/icons-material/Search";
+import CancelIcon from "@mui/icons-material/Close"
 
 function SearchBar(){
     
@@ -28,13 +29,25 @@ function SearchBar(){
         navigation("/searchResult?q="+search, { state: { search } });
     };
 
+    const onCancel = () => {
+        setSearch("");
+    };
+
+
     return (
         <div className={styles.container}>
              <input className={styles.searchBar} type="text" placeholder="노래 검색" value={search} onChange={onChange} onKeyDown={(e) => { if(e.key === "Enter") onSubmit(); }}/>
+             {search && ( 
+                <CancelIcon
+                    className={styles.cancelIcon}
+                    onClick={onCancel}
+                    style={{ position: "absolute", right: "2rem", top: "50%", transform: "translateY(-50%)", color:"gray"}}
+                />
+            )}
              <SearchIcon
           className={styles.searchIcon}
           onClick={onSubmit}
-          style={{ position: "absolute", right: "10px", top: "50%", transform: "translateY(-50%)" }}
+          style={{ position: "absolute", right: "0.5rem", top: "50%", transform: "translateY(-50%)" }}
         />
         </div>
     )
