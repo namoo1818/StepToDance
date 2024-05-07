f74fc14f84969d631302f8bc69fb60bb60fd77a5
Jinha Hwang <hjh70902000@gmail.com>
2024-05-08 01:13:36 +0900

    Fix: objectmapper 빈 주입 수정
    Former-commit-id: 618b9541abe0cc29513927f6303e8da9a4c18321

--- BE/steptodance/src/main/java/com/dance101/steptodance/infra/KafkaService.java
+++ BE/steptodance/src/main/java/com/dance101/steptodance/infra/KafkaService.java
@@ -14,6 +14,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.kafka.annotation.KafkaListener;
@@ -30,6 +31,7 @@ public class KafkaService implements AIServerService {
     private String feedbackTopicName;
     @Value(value = "${message.topic.guide.name}")
     private String guideTopicName;
+    @Autowired
     private final ObjectMapper objectMapper;
 
     @Override
