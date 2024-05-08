01b27f83552ced4c51b984af2cabce05e981a707
황진하 <hjh70902000@naver.com>
2024-05-08 16:45:49 +0900

    Fix: 배열이 아닌 json을 반환하도록 변경
    Former-commit-id: 28569b84c8d066aaf6d9ea2f44255494c3e385a1

--- AI/src/util/AiUtil.py
+++ AI/src/util/AiUtil.py
@@ -43,7 +43,32 @@ def imgToBodyModel(image):
 
     # 결과 받아오기
     output = net.forward()
-    
-    print('output', json.dumps(output))
+    # output.shape[0] = 이미지 ID, [1] = 출력 맵의 높이, [2] = 너비
+    H = output.shape[2]
+    W = output.shape[3]
+    print("이미지 ID : ", len(output[0]), ", H : ", output.shape[2], ", W : ",output.shape[3]) # 이미지 ID
 
-    return json.dumps(output)
+    # 키포인트 검출시 이미지에 그려줌
+    points = []
+    for i in range(0,15):
+        # 해당 신체부위 신뢰도 얻음.
+        probMap = output[0, i, :, :]
+ 
+       # global 최대값 찾기
+        minVal, prob, minLoc, point = cv2.minMaxLoc(probMap)
+
+        # 원래 이미지에 맞게 점 위치 변경
+        x = (imageWidth * point[0]) / W
+        y = (imageHeight * point[1]) / H
+
+        # 키포인트 검출한 결과가 0.1보다 크면(검출한곳이 위 BODY_PARTS랑 맞는 부위면) points에 추가, 검출했는데 부위가 없으면 None으로    
+        if prob > 0.1 :    
+            cv2.circle(image, (int(x), int(y)), 3, (0, 255, 255), thickness=-1, lineType=cv2.FILLED)       # circle(그릴곳, 원의 중심, 반지름, 색)
+            cv2.putText(image, "{}".format(i), (int(x), int(y)), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1, lineType=cv2.LINE_AA)
+            points.append((int(x), int(y)))
+        else :
+            points.append(None)
+    
+    print('output', points)
+
+    return points
