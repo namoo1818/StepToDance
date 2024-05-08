167ce8f6538a7100543e94163bd30a019cf9b777
황진하 <hjh70902000@naver.com>
2024-05-08 13:40:12 +0900

    Fix: redis 위치 변경
    Former-commit-id: afe4fd5cddc9ff0c8f8e8e2a83b85b1b56290960

--- AI/src/service/GuideService.py
+++ AI/src/service/GuideService.py
@@ -1,13 +1,11 @@
 import boto3
 from data.GuideRequest import GuideUpdateRequest
 from data.GuideUpdateMsg import GuideUpdateMsg
-from core.redis_config import redis_config
 from util.AiUtil import imgToBodyModel
 
 # TODO: env로 키를 옮기기
 AWS_S3_ACCESS_KEY = "test"
 AWS_S3_PRIVATE_KEY = "test"
-redis = redis_config()
 
 def guideUpload(video_url: str):
     s3 = boto3.client("s3", aws_access_key_id=AWS_S3_ACCESS_KEY, aws_secret_access_key=AWS_S3_PRIVATE_KEY)
