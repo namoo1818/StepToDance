bd38acefc5f41a3cee2901199b68233b0e5c1507
황진하 <hjh70902000@naver.com>
2024-05-08 14:51:18 +0900

    Feat: AI 모델 테스트
    Former-commit-id: ca017a5296ce45fa1c1b4c562d16d861e67798db

--- AI/src/util/AiUtil.py
+++ AI/src/util/AiUtil.py
@@ -15,4 +15,16 @@ def get_s3_client():
 
 def imgToBodyModel(image) -> list:
     print("imgToBodyModel: " , image[:10])
+    # 각 파일 path
+    # protoFile = "pose_deploy_linevec_faster_4_stages.prototxt"
+    protoFile = "resource\model\pose_deploy_linevec_faster_4_stages.prototxt"
+    # weightsFile = "pose_iter_160000.caffemodel"
+    weightsFile = "resource\model\pose_iter_160000.caffemodel"
+ 
+    # 위의 path에 있는 network 불러오기
+    net = cv2.dnn.readNetFromCaffe(protoFile, weightsFile)
+
+    image = cv2.imdecode(image)
+    print(image)
+
     return None
