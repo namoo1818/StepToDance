a30444de76d265bb7e5528fc62b384073bf0c328
황진하 <hjh70902000@naver.com>
2024-05-08 15:21:57 +0900

    Fix: 경로 오타 해결
    Former-commit-id: 655232435c15ac51f8e58eea57cec4900d151c3f

--- AI/src/util/AiUtil.py
+++ AI/src/util/AiUtil.py
@@ -17,9 +17,9 @@ def imgToBodyModel(image) -> list:
     print("imgToBodyModel: " , image[:10])
     # 각 파일 path
     # protoFile = "pose_deploy_linevec_faster_4_stages.prototxt"
-    protoFile = "../resource/model/pose_deploy_linevec_faster_4_stages.prototxt"
+    protoFile = "../resources/model/pose_deploy_linevec_faster_4_stages.prototxt"
     # weightsFile = "pose_iter_160000.caffemodel"
-    weightsFile = "../resource/model/pose_iter_160000.caffemodel"
+    weightsFile = "../resources/model/pose_iter_160000.caffemodel"
  
     # 위의 path에 있는 network 불러오기
     net = cv2.dnn.readNetFromCaffe(protoFile, weightsFile)
