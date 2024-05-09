db737dbf5ba384585e2fcd40b25097286f2fdd6e
Jinha Hwang <hjh70902000@gmail.com>
2024-05-09 16:16:25 +0900

    Fix: 가이드 완료 신호를 받을 시 모델 처리 후 mongoDB 업로드
    Former-commit-id: 5b3141bfc73dfd405ae4d379856362b3cc2e206a

--- BE/steptodance/src/main/java/com/dance101/steptodance/infra/KafkaService.java
+++ BE/steptodance/src/main/java/com/dance101/steptodance/infra/KafkaService.java
@@ -2,12 +2,18 @@ package com.dance101.steptodance.infra;
 
 import static com.dance101.steptodance.global.exception.data.response.ErrorCode.*;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import com.dance101.steptodance.feedback.service.FeedbackService;
 import com.dance101.steptodance.global.exception.category.ExternalServerException;
-import com.dance101.steptodance.global.exception.data.response.ErrorCode;
 import com.dance101.steptodance.guide.data.request.FeedbackMessageRequest;
+import com.dance101.steptodance.guide.data.request.GuideFrame;
 import com.dance101.steptodance.guide.data.request.GuideMessageRequest;
 import com.dance101.steptodance.guide.data.response.GuideFeedbackCreateResponse;
+import com.dance101.steptodance.guide.domain.GuideBodyModel;
+import com.dance101.steptodance.guide.repository.GuideBodyRepository;
+import com.dance101.steptodance.guide.repository.GuideRepository;
 import com.dance101.steptodance.guide.service.AIServerService;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -16,16 +22,18 @@ import lombok.extern.slf4j.Slf4j;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Slf4j
 @RequiredArgsConstructor
 @Service
 public class KafkaService implements AIServerService {
     private final FeedbackService feedbackService;
+    private final GuideBodyRepository guideBodyRepository;
     private final KafkaTemplate<String, String> kafkaTemplate;
     @Value(value = "${message.topic.feedback.name}")
     private String feedbackTopicName;
@@ -33,6 +41,8 @@ public class KafkaService implements AIServerService {
     private String guideTopicName;
     @Autowired
     private final ObjectMapper objectMapper;
+    private final StringRedisTemplate redisTemplate;
+    private final List<Integer> zeroList = List.of(0, 0);
 
     @Override
     public void publish(GuideMessageRequest guideMessageRequest) {
@@ -55,10 +65,111 @@ public class KafkaService implements AIServerService {
     @KafkaListener(topics = "${message.topic.name}", groupId = "step-to-dance")
     public void consume(String message) throws JsonProcessingException {
         // convert message to DTO
-        ObjectMapper mapper = new ObjectMapper();
-        GuideFeedbackCreateResponse response = mapper.readValue(message, GuideFeedbackCreateResponse.class);
+        GuideFeedbackCreateResponse response = objectMapper.readValue(message, GuideFeedbackCreateResponse.class);
 
         // update & save result
-        feedbackService.updateFeedback(response);
+        // feedbackService.updateFeedback(response);
+    }
+
+    @Transactional
+    @KafkaListener(topics = "${message.topic.guide.completion-name}", groupId = "step-to-dance")
+    public void consumeGuideCompletion(String message) throws JsonProcessingException {
+        // call guide model & save to MongoDB
+        List<String> list = redisTemplate.opsForList().range("guide:"+message, 0, -1);
+        List<GuideFrame> frameList = list.parallelStream().map(item -> {
+            try {
+                return objectMapper.readValue(item, GuideFrame.class);
+            } catch (JsonProcessingException e) {
+                throw new RuntimeException(e);
+            }
+        }).toList();
+
+        // TODO: 정렬이 안된다면 위의 리스트를 sort 가능한 리스트로 변환
+        frameList.sort((item1, item2) -> item1.getName().compareTo(item2.getName()));
+        // 첫 프레임 채우기
+        GuideFrame frame = frameList.get(0);
+        for (int i = 0; i < frame.getModel().size(); i++) {
+            if (frame.getModel().get(i) == null) {
+                dfsBothEnd(i, 1, '+', frameList);
+            }
+        }
+
+        // 마지막 프레임 채우기
+        frame = frameList.get(frameList.size()-1);
+        for (int i = 0; i < frame.getModel().size(); i++) {
+            if (frame.getModel().get(i) == null) {
+                dfsBothEnd(i, frameList.size() - 2, '-', frameList);
+            }
+        }
+
+        // 중간 프레임 채우기
+        for (int i = 1; i < frameList.size() - 1; i++) {
+            for (int j = 0; j < frame.getModel().size(); j++) {
+                fillFrame(j, i, frameList);
+            }
+        }
+
+        // update & save result
+        GuideBodyModel model = GuideBodyModel.builder()
+            .guideId(Long.parseLong(message))
+            .models(
+                frameList.stream()
+                    .map(GuideFrame::getModel)
+                    .collect(Collectors.toList())
+            )
+            .build();
+
+        guideBodyRepository.save(model);
+
+        // TODO: 레디스에 저장된 내용을 지운다.
+        // redisTemplate.delete("guide:" + message);
+    }
+
+    private void fillFrame(int joint, int frameIndex, List<GuideFrame> frameList) {
+        int startFrameIndex = frameIndex - 1;
+        int endFrameIndex = frameIndex;
+        while (frameList.get(endFrameIndex).getModel().get(joint) == null) {
+            endFrameIndex++;
+        }
+        // 끝날때 까지 비어있다면
+        if (endFrameIndex == frameList.size()) {
+            for (int i = frameIndex; i < endFrameIndex; i++) {
+                frameList.get(i).getModel().set(joint, zeroList);
+            }
+            return;
+        }
+        int diffY = frameList.get(endFrameIndex).getModel().get(joint).get(0)
+            - frameList.get(startFrameIndex).getModel().get(joint).get(0);
+        diffY /= endFrameIndex - startFrameIndex;
+        int diffX = frameList.get(endFrameIndex).getModel().get(joint).get(1)
+            - frameList.get(startFrameIndex).getModel().get(joint).get(1);
+        diffX /= endFrameIndex - startFrameIndex;
+
+        List<Integer> last = frameList.get(startFrameIndex).getModel().get(joint);
+        List<Integer> curr;
+        startFrameIndex++;
+        for (int i = startFrameIndex; i < endFrameIndex; i++) {
+            curr = List.of(last.get(0)+diffY, last.get(1)+diffX);
+            frameList.get(i).getModel().set(joint, curr);
+            last = curr;
+        }
+    }
+
+    private List<Integer> dfsBothEnd(int i, int depth, char cmd, List<GuideFrame> frameList) {
+        if (depth >= frameList.size() || depth < 0) return zeroList;
+        List<Integer> thisFrame = frameList.get(depth).getModel().get(i);
+        if (thisFrame != null) {
+            return thisFrame;
+        }
+        else {
+            if (cmd == '+') {
+                frameList.get(depth).getModel().set(i, dfsBothEnd(i, depth + 1, cmd, frameList));
+                return thisFrame;
+            }
+            else {
+                frameList.get(depth).getModel().set(i, dfsBothEnd(i, depth - 1, cmd, frameList));
+                return thisFrame;
+            }
+        }
     }
 }
