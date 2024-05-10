01981d98b028e86959c0c8941157f98f110893dd
황진하 <hjh70902000@naver.com>
2024-05-10 15:24:17 +0900

    Fix: 이미지문자열이 너무 길어서 로그가 안보이는 문제 수정
    Former-commit-id: e1a409cc94edfd675f3f1b89dc983e49eb5e6ff5

--- AI/src/data/FeedbackUpdateMsg.py
+++ AI/src/data/FeedbackUpdateMsg.py
@@ -5,7 +5,6 @@ class FeedbackUpdateMsg():
     image: bytes
 
     def __init__(self, msgInstance: dict):
-        print("피드백 dict -> 객체", str(msgInstance))
         self.feedbackId = msgInstance['id']
         self.name = msgInstance['name']
         self.size = msgInstance['size']
--- AI/src/data/GuideUpdateMsg.py
+++ AI/src/data/GuideUpdateMsg.py
@@ -5,7 +5,6 @@ class GuideUpdateMsg():
     image: bytes
 
     def __init__(self, msgInstance: dict):
-        print("가이드 dict -> 객체", str(msgInstance))
         self.guideId = msgInstance['id']
         self.name = msgInstance['name']
         self.size = msgInstance['size']
