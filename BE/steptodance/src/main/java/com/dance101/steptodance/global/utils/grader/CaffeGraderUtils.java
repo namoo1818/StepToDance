cd1cc6d10c8893d1f392fa0731826e5aadb59598
황진하 <hjh70902000@naver.com>
2024-05-13 09:47:34 +0900

    Fix: 소숫점 연산을 BigDecimal로 정확하게 수정
    Former-commit-id: 9c215d51805d30671fecef10b69cbbabdc290ed0

--- BE/steptodance/src/main/java/com/dance101/steptodance/global/utils/grader/CaffeGraderUtils.java
+++ BE/steptodance/src/main/java/com/dance101/steptodance/global/utils/grader/CaffeGraderUtils.java
@@ -1,5 +1,8 @@
 package com.dance101.steptodance.global.utils.grader;
 
+import java.math.BigDecimal;
+import java.math.MathContext;
+import java.math.RoundingMode;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -13,6 +16,7 @@ import lombok.extern.slf4j.Slf4j;
 public class CaffeGraderUtils implements GraderUtils{
 	private List<List<Integer>> gradingCriteria;
 	private final int SIZE_OF_CRITERIA = 13;
+	private final MathContext mc = new MathContext(7, RoundingMode.HALF_UP);
 
 	public CaffeGraderUtils() {
 		gradingCriteria = new ArrayList<>();
@@ -46,36 +50,36 @@ public class CaffeGraderUtils implements GraderUtils{
 	}
 
 	private double getAngle(int ci, List<List<Integer>> body) {
-		double y1 = body.get(gradingCriteria.get(ci).get(0)).get(0);
-		double x1 = body.get(gradingCriteria.get(ci).get(0)).get(1);
-		double y2 = body.get(gradingCriteria.get(ci).get(1)).get(0);
-		double x2 = body.get(gradingCriteria.get(ci).get(1)).get(1);
-		double y3 = body.get(gradingCriteria.get(ci).get(2)).get(0);
-		double x3 = body.get(gradingCriteria.get(ci).get(2)).get(1);
+		int y1 = body.get(gradingCriteria.get(ci).get(0)).get(0);
+		int x1 = body.get(gradingCriteria.get(ci).get(0)).get(1);
+		int y2 = body.get(gradingCriteria.get(ci).get(1)).get(0);
+		int x2 = body.get(gradingCriteria.get(ci).get(1)).get(1);
+		int y3 = body.get(gradingCriteria.get(ci).get(2)).get(0);
+		int x3 = body.get(gradingCriteria.get(ci).get(2)).get(1);
 
-		double vector1X = x2 - x1;
-		double vector1Y = y2 - y1;
-		double vector2X = x3 - x2;
-		double vector2Y = y3 - y2;
+		BigDecimal vector1X = new BigDecimal(x2 - x1);
+		BigDecimal vector1Y = new BigDecimal(y2 - y1);
+		BigDecimal vector2X = new BigDecimal(x3 - x2);
+		BigDecimal vector2Y = new BigDecimal(y3 - y2);
 
-		double dotProduct = vector1X * vector2X + vector1Y * vector2Y;
-		double vector1Length = Math.sqrt(Math.abs(vector1X * vector1X + vector1Y * vector1Y));
-		vector1Length = vector1Length == 0? 0.0001 : vector1Length;
-		double vector2Length = Math.sqrt(Math.abs(vector2X * vector2X + vector2Y * vector2Y));
-		vector2Length = vector2Length == 0? 0.0001 : vector2Length;
+		BigDecimal dotProduct = vector1X.multiply(vector2X ).add(vector1Y.multiply(vector2Y));
+		BigDecimal vector1Length = vector1X.multiply(vector1X).add(vector1Y.multiply(vector1Y)).sqrt(mc);
+		vector1Length = vector1Length.doubleValue() == 0.0 ? new BigDecimal("0.0000001") : vector1Length;
+		BigDecimal vector2Length = vector2X.multiply(vector2X).add(vector2Y.multiply(vector2Y)).sqrt(mc);
+		vector2Length = vector2Length.doubleValue() == 0.0 ? new BigDecimal("0.0000001") : vector2Length;
 
-		if (dotProduct / (vector1Length * vector2Length) > 1.0 || dotProduct / (vector1Length * vector2Length) < -1.0) {
+		if (dotProduct.divide(vector1Length.multiply(vector2Length), mc).doubleValue() > 1.0 || dotProduct.divide(vector1Length.multiply(vector2Length), mc).doubleValue() < -1.0) {
 			log.info("================================ problem occurred! ================================");
 			log.info("{y1, x1, y2, x2, y3, x3} : " + Arrays.toString(new double[] {y1, x1, y2, x2, y3, x3}));
-			log.info("{vector1Y, vector1X, vector2Y, vector2X} : " + Arrays.toString(new double[] {vector1Y, vector1X, vector2Y, vector2X}));
+			log.info("{vector1Y, vector1X, vector2Y, vector2X} : " + Arrays.toString(new double[] {vector1Y.doubleValue(), vector1X.doubleValue(), vector2Y.doubleValue(), vector2X.doubleValue()}));
 			log.info("getAngle: dotProduct= " + dotProduct);
 			log.info("getAngle: vector1Length= " + vector1Length);
 			log.info("getAngle: vector2Length= " + vector2Length);
-			log.info("getAngle: (vector1Length * vector2Length)= " + (vector1Length * vector2Length));
-			log.info("getAngle: dotProduct / (vector1Length * vector2Length)= " + dotProduct / (vector1Length * vector2Length));
+			log.info("getAngle: (vector1Length * vector2Length)= " + (vector1Length.multiply(vector2Length)));
+			log.info("getAngle: dotProduct / (vector1Length * vector2Length)= " + dotProduct.divide(vector1Length.multiply(vector2Length), mc).doubleValue());
 		}
 		// 라디안 각도를 반환
-		double acos = Math.acos(dotProduct / (vector1Length * vector2Length));
+		double acos = Math.acos(dotProduct.divide(vector1Length.multiply(vector2Length), mc).doubleValue());
 		return acos;
 	}
 }
