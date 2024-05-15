31422073d831e9fa18d1d9750f294b546a7c058d
황진하 <hjh70902000@naver.com>
2024-05-15 21:42:11 +0900

    Fix: 프레임에 따른 썸네일 파일명 수정
    Former-commit-id: 05189588cdc7898fbe9dd471648f50e8ad7e6f18

--- BE/steptodance/src/main/java/com/dance101/steptodance/global/utils/FFmpegUtils.java
+++ BE/steptodance/src/main/java/com/dance101/steptodance/global/utils/FFmpegUtils.java
@@ -185,7 +185,7 @@ public class FFmpegUtils {
 		log.info(type + " id: " + id + ", frame amount: " + size);
 
 		MultipartFile ret = FileUtil.convertToMultipartFile(
-			new File(outputDirPath + type + id + "/frame_00090.png"));
+			new File(outputDirPath + type + id + "/frame_00006.png"));
 
 		// 이미지파일 삭제
 		Files.walk(Path.of(outputDirPath + type + id))
