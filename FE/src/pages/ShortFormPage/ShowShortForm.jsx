cdf80ff62bab87e2d6c5525e9cddb89d40653025
choikeunyoung <ought119@gmail.com>
2024-05-15 16:57:29 +0900

    Feat: 스크롤 한 영역씩
    Former-commit-id: 7e1c5025da365180cb5e81dfa4a08db5b68741ba

--- FE/src/pages/ShortFormPage/ShowShortForm.jsx
+++ FE/src/pages/ShortFormPage/ShowShortForm.jsx
@@ -1,11 +1,88 @@
 import styles from "./ShowShortForm.module.css";
 import testVideo from "../../assets/PerfectNight_르세라핌.mp4";
 import { getShortformList } from "../../api/ShortformApis";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 const ShowShortForm = () => {
   const [showShortForm, setShowShortForm] = useState([]);
   const [renderVideo, setRenderVideo] = useState([]);
+
+  const outerDivRef = useRef();
+  useEffect(() => {
+    const wheelHandler = (e) => {
+      e.preventDefault();
+      const { deltaY } = e;
+      const { scrollTop } = outerDivRef.current;
+      const pageHeight = outerDivRef.current.offsetHeight;
+
+      if (deltaY > 0) {
+        // 스크롤 내릴 때
+        if (scrollTop >= 0 && scrollTop < pageHeight) {
+          outerDivRef.current.scrollTo({
+            top: pageHeight,
+            left: 0,
+            behavior: "smooth",
+          });
+        } else if (scrollTop >= pageHeight && scrollTop < pageHeight * 2) {
+          outerDivRef.current.scrollTo({
+            top: pageHeight * 2 + 5,
+            left: 0,
+            behavior: "smooth",
+          });
+        } else if (scrollTop >= pageHeight * 2 && scrollTop < pageHeight * 3) {
+          outerDivRef.current.scrollTo({
+            top: pageHeight * 3 + 6,
+            left: 0,
+            behavior: "smooth",
+          });
+        } else {
+          outerDivRef.current.scrollTo({
+            top: pageHeight * 4 + 11,
+            left: 0,
+            behavior: "smooth",
+          });
+        }
+      } else {
+        // 스크롤 올릴 때
+        if (scrollTop >= 0 && scrollTop < pageHeight) {
+          outerDivRef.current.scrollTo({
+            top: 0,
+            left: 0,
+            behavior: "smooth",
+          });
+        } else if (scrollTop >= pageHeight && scrollTop < pageHeight * 2) {
+          outerDivRef.current.scrollTo({
+            top: 0,
+            left: 0,
+            behavior: "smooth",
+          });
+        } else if (scrollTop >= pageHeight * 2 && scrollTop < pageHeight * 3) {
+          outerDivRef.current.scrollTo({
+            top: pageHeight + 5,
+            left: 0,
+            behavior: "smooth",
+          });
+        } else if (scrollTop >= pageHeight * 3 && scrollTop < pageHeight * 4) {
+          outerDivRef.current.scrollTo({
+            top: pageHeight * 2 + 6,
+            left: 0,
+            behavior: "smooth",
+          });
+        } else {
+          outerDivRef.current.scrollTo({
+            top: pageHeight * 3 + 11,
+            left: 0,
+            behavior: "smooth",
+          });
+        }
+      }
+    };
+    const outerDivRefCurrent = outerDivRef.current;
+    outerDivRefCurrent.addEventListener("wheel", wheelHandler);
+    return () => {
+      outerDivRefCurrent.removeEventListener("wheel", wheelHandler);
+    };
+  }, []);
 
   useEffect(() => {
     const getData = async () => {
@@ -28,7 +105,11 @@ const ShowShortForm = () => {
     setRenderVideo(videoList);
   }, [showShortForm]);
 
-  return <section className={styles["short-page"]}>{renderVideo}</section>;
+  return (
+    <section className={styles["short-page"]} ref={outerDivRef}>
+      {renderVideo}
+    </section>
+  );
 };
 
 export default ShowShortForm;
