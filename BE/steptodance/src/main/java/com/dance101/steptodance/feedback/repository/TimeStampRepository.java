8108b9c0867a30de84ae99b12dd6f7840645bec3
황진하 <hjh70902000@naver.com>
2024-05-16 00:44:36 +0900

    Feat: 피드백 삭제가 타임스탬프를 지우도록
    Former-commit-id: c9257511adb911b81f66b5cc277774f027e21dc2

--- BE/steptodance/src/main/java/com/dance101/steptodance/feedback/repository/TimeStampRepository.java
+++ BE/steptodance/src/main/java/com/dance101/steptodance/feedback/repository/TimeStampRepository.java
@@ -1,7 +1,9 @@
 package com.dance101.steptodance.feedback.repository;
 
+import com.dance101.steptodance.feedback.domain.Feedback;
 import com.dance101.steptodance.feedback.domain.Timestamp;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TimeStampRepository extends JpaRepository<Timestamp, Long> {
+	void deleteAllByfeedback(Feedback feedback);
 }
