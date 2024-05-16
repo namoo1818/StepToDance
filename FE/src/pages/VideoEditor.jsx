749ff2574b1d4b529689cdb6103ecaca708fa6d6
최근영 <wwaall2626@naver.com>
2024-05-16 17:30:47 +0900

    Fix: 로그인 테스트
    Former-commit-id: 2507e6fe97801532f233f7fb197bbad323354f0d

--- FE/src/pages/VideoEditor.jsx
+++ FE/src/pages/VideoEditor.jsx
@@ -1,11 +1,11 @@
-import {useState,useEffect,useRef} from "react";
+import { useState, useEffect, useRef } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import ReactPlayer from "react-player";
 import styles from "../styles/VideoEditor.module.css";
 import PlayArrowIcon from "@mui/icons-material/PlayArrow";
 import PauseIcon from "@mui/icons-material/Pause";
 import { uploadShortform } from "../api/ShortformApis";
-import TimeRange from 'react-timeline-range-slider';
+import TimeRange from "react-timeline-range-slider";
 import { format } from "date-fns";
 
 function VideoEditor() {
@@ -14,7 +14,9 @@ function VideoEditor() {
   const state = location.state;
   const [guideId, setGuideId] = useState(state.guideId);
   const [videoUrl, setVideoUrl] = useState(state.videoUrl);
-  const [highlightStartAt, setHighlightStartAt] = useState(state.highlightStartAt);
+  const [highlightStartAt, setHighlightStartAt] = useState(
+    state.highlightStartAt
+  );
   const [highlightEndAt, setHighlightEndAt] = useState(state.highlightEndAt);
   const [played, setPlayed] = useState(0);
   // const [startAt, setStartAt] = useState(convertTimeFormat(state.highlightStartAt));
@@ -29,13 +31,10 @@ function VideoEditor() {
 
   const [selectedInterval, setSelectedInterval] = useState([
     new Date(convertTimeFormat(highlightStartAt)),
-    new Date(convertTimeFormat(highlightEndAt))
+    new Date(convertTimeFormat(highlightEndAt)),
   ]);
 
-  const timeline = [
-    new Date(0),
-    new Date(67*1000)
-  ];
+  const timeline = [new Date(0), new Date(67 * 1000)];
 
   const handlePlayPause = () => {
     if (ended) {
@@ -70,37 +69,40 @@ function VideoEditor() {
 
   function convertTimeFormat(input) {
     // 문자열인 경우 ms 객체로 변환
-    if (typeof input === 'string') {
-      const [hours, minutes, seconds] = input.split(':');
-      const milliseconds = (parseInt(hours, 10) * 3600 + parseInt(minutes, 10) * 60 + parseInt(seconds, 10)) * 1000;
+    if (typeof input === "string") {
+      const [hours, minutes, seconds] = input.split(":");
+      const milliseconds =
+        (parseInt(hours, 10) * 3600 +
+          parseInt(minutes, 10) * 60 +
+          parseInt(seconds, 10)) *
+        1000;
       return milliseconds;
-  }
+    }
     // Date 객체인 경우 문자열로 변환
     else if (input instanceof Date) {
-        const hours = ('0' + input.getHours()).slice(-2);
-        const minutes = ('0' + input.getMinutes()).slice(-2);
-        const seconds = ('0' + input.getSeconds()).slice(-2);
-        return `${hours}:${minutes}:${seconds}`;
+      const hours = ("0" + input.getHours()).slice(-2);
+      const minutes = ("0" + input.getMinutes()).slice(-2);
+      const seconds = ("0" + input.getSeconds()).slice(-2);
+      return `${hours}:${minutes}:${seconds}`;
     }
     // 다른 형식인 경우 에러 반환
     else {
-        throw new Error('Unsupported input format');
+      throw new Error("Unsupported input format");
     }
-}
-
+  }
 
   const createShortform = async () => {
-    console.log("시작:",convertTimeFormat(selectedInterval[0]));
-    console.log("끝:",convertTimeFormat(selectedInterval[1]));
+    console.log("시작:", convertTimeFormat(selectedInterval[0]));
+    console.log("끝:", convertTimeFormat(selectedInterval[1]));
     const response = await uploadShortform(
       guideId,
       videoUrl,
       convertTimeFormat(selectedInterval[0]),
-      convertTimeFormat(selectedInterval[1]),
+      convertTimeFormat(selectedInterval[1])
     );
-    console.log('Shortform created successfully:', response);
+    console.log("Shortform created successfully:", response);
     navigate(`/shortsShare?id=${response.data}`);
-  }
+  };
 
   // const handleInputChange = (index, newValue) => {
   //   if(index==="start") setStartAt(newValue);
@@ -110,9 +112,9 @@ function VideoEditor() {
   const reset = () => {
     setSelectedInterval([
       new Date(convertTimeFormat(highlightStartAt)),
-      new Date(convertTimeFormat(highlightEndAt))
+      new Date(convertTimeFormat(highlightEndAt)),
     ]);
-  }
+  };
 
   const timelineScrubberErrorHandler = ({ error }) => {
     setTimelineScrubberError(error);
@@ -126,12 +128,8 @@ function VideoEditor() {
   return (
     <div className={styles.homeContainer}>
       <div>
-        <button onClick={()=>reset}>
-          원본으로 복원
-        </button>
-        <button onClick={createShortform}>
-          완료
-        </button>
+        <button onClick={() => reset}>원본으로 복원</button>
+        <button onClick={createShortform}>완료</button>
       </div>
       <div>
         <ReactPlayer
@@ -179,12 +177,12 @@ function VideoEditor() {
           ></div>
         </div>
       </div>
-      <div style={{marginTop:'5vw'}}>
+      {/* <div style={{marginTop:'5vw'}}>
         <p style={{color:'white'}}>시작 시간</p>
         <input type="text" value={startAt} onChange={(e) => handleInputChange('start', e.target.value)}/>
         <p style={{color:'white'}}>끝 시간</p>
         <input type="text" value={endAt} onChange={(e) => handleInputChange( 'end', e.target.value)}/>
-      </div>
+      </div> */}
       <div>
         <TimeRange
           showNow
