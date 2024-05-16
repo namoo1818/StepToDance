cbb2301560722cb68771f5ab9ed44de1407c0296
백지윤 <wendy6992@naver.com>
2024-05-16 14:54:27 +0900

    Feat: Home 화면 css 수정
    Former-commit-id: 5706723954180e9becfe75e7595d69f4a3ef57f6

--- FE/src/components/guide/GuideList.jsx
+++ FE/src/components/guide/GuideList.jsx
@@ -3,7 +3,6 @@ import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import styles from "../../styles/guide/GuideList.module.css";
 import { getGuideList } from "../../api/GuideApis";
-import styled from "styled-components";
 import Button from "../CustomButton";
 
 function GuideList() {
@@ -27,28 +26,24 @@ function GuideList() {
     fetchGuideData();
   }, [genre]);
 
-  useEffect(()=> {
-    if(genre==='k-pop'){
+  useEffect(() => {
+    if (genre === 'k-pop') {
       setKrGenre('케이팝');
-    }
-    else if(genre==='B-boying'){
+    } else if (genre === 'B-boying') {
       setKrGenre('비보잉');
-    }
-    else if(genre==='hip-hop'){
+    } else if (genre === 'hip-hop') {
       setKrGenre('힙합');
-    }
-    else if(genre==='popping'){
+    } else if (genre === 'popping') {
       setKrGenre('팝핀');
-    }
-    else if(genre==='korean traditional dance'){
+    } else if (genre === 'korean traditional dance') {
       setKrGenre('전통무용');
     }
-  },[genre]);
+  }, [genre]);
 
   const renderItem = ({ item }) => (
-    <Link to={{ pathname: `/guideDetail/${item.id}` }}>
+    <Link to={{ pathname: `/guideDetail/${item.id}` }} className={styles.link}>
       <div className={styles.guide}>
-        <img className={styles.image} src={item.thumbnail_img_url} />
+        <img className={styles.image} src={item.thumbnail_img_url} alt="thumbnail" />
         <p className={styles.text}>
           {item.song_title} - {item.singer}
         </p>
@@ -56,34 +51,62 @@ function GuideList() {
     </Link>
   );
 
-    const choiceGenre = (keyword) => {
-      console.log(keyword);
-        setGenre(keyword);
+  const choiceGenre = (keyword) => {
+    console.log(keyword);
+    setGenre(keyword);
+  }
+
+  useEffect(() => {
+    const nav = document.getElementById("js-nav");
+    const pointer = document.getElementById("js-pointer");
+    const links = nav.getElementsByTagName("a");
+
+    pointer.style.width = `calc(100% / ${links.length} - 0.5em)`;
+    pointer.style.height = `calc(60%)`;
+
+
+    for (let i = 0; i < links.length; i++) {
+      const current = links[i];
+      current.dataset.order = i * 100 + "%";
+      current.addEventListener("click", movePointer);
     }
+
+    function movePointer(e) {
+      const order = e.currentTarget.dataset.order;
+      pointer.style.transform = `translate3d(${order},0,0)`;
+    }
+  }, []);
 
   return (
     <div className={styles.container}>
-      <div className={styles.buttons}>
-        <Button variant="success" size="sm" onClick={() => choiceGenre('k-pop')} selected={genre === 'k-pop'}>케이팝</Button>
-        <Button variant="success" size="sm" onClick={() => choiceGenre('B-boying')} selected={genre === 'B-boying'}>비보잉</Button>
-        <Button variant="success" size="sm" onClick={() => choiceGenre('hip-hop')} selected={genre === 'hip-hop'}>힙합</Button>
-        <Button variant="success" size="sm" onClick={() => choiceGenre('popping')} selected={genre === 'popping'}>팝핀</Button>
-        <Button variant="success" size="sm" onClick={() => choiceGenre('korean traditional dance', '전통무용')} selected={genre === 'korean traditional dance'}>전통무용</Button>
-      </div>
       <div className={styles.title}>{user.nickname}님이 좋아하는 {krGenre} 장르</div>
-      <Links>
+      <nav className={styles.nav} id="js-nav">
+        <div id="js-pointer" className={styles.nav__pointer}></div>
+        <ul className={styles.nav__list}>
+          <li>
+            <a href="#" onClick={() => choiceGenre('k-pop')} data-order="0%">케이팝</a>
+          </li>
+          <li>
+            <a href="#" onClick={() => choiceGenre('B-boying')} data-order="100%">비보잉</a>
+          </li>
+          <li>
+            <a href="#" onClick={() => choiceGenre('hip-hop')} data-order="200%">힙합</a>
+          </li>
+          <li>
+            <a href="#" onClick={() => choiceGenre('popping')} data-order="300%">팝핀</a>
+          </li>
+          <li>
+            <a href="#" onClick={() => choiceGenre('korean traditional dance')} data-order="400%">전통 무용</a>
+          </li>
+        </ul>
+      </nav>
+      <div className={styles.links}>
         {guideList.map((item) => (
           <div key={`page_${item.id}`}>{renderItem({ item })}</div>
         ))}
-      </Links>
+      </div>
     </div>
   );
 }
-
-const Links = styled.div`
-  a {
-    text-decoration: none;
-  }
-`;
 
 export default GuideList;
