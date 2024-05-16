683fae0a025f11f20f30188bf09822650d6bc653
백지윤 <wendy6992@naver.com>
2024-05-16 12:41:01 +0900

    Feat: 장르 수정
    Former-commit-id: 85acbc4d90d9ceb741c18b01559656b9b86f582f

--- FE/src/pages/LoadingPage/LoadingPage.jsx
+++ FE/src/pages/LoadingPage/LoadingPage.jsx
@@ -2,14 +2,11 @@ import React from 'react';
 import styles from './LoadingPage.module.css'; // Import your CSS module
 import Models from '../../components/Models/Models';
 
-const LoadingPage = ({ progress }) => {
+const LoadingPage = () => {
   return (
     <div className={styles.loadingPage}>
       <Models/>
-      <div className={styles.progressBarContainer}>
-        <div className={styles.progressBar} style={{ width: `${progress}%` }}></div>
-      </div>
-      <p>Uploading... {progress}%</p>
+      <p>업로드중</p>
     </div>
   );
 };
