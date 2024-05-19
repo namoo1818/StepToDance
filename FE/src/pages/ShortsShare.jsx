9c99486d0f61683ea1cd0f6314588f98f73d7597
최근영 <ought119@gmail.com>
2024-05-19 20:58:37 +0900

    Fix: 별똥별 효과 적용
    Former-commit-id: 062f1389fbe2731f823877b919b97fd6049ef9c7

--- FE/src/pages/ShortsShare.jsx
+++ FE/src/pages/ShortsShare.jsx
@@ -1,85 +1,83 @@
 import { useLocation } from "react-router-dom";
 import { useEffect, useState, useRef } from "react";
 import { getShortformDetail } from "../api/ShortformApis";
-import ReactPlayer from 'react-player';
-import PlayArrowIcon from '@mui/icons-material/PlayArrow';
-import PauseIcon from '@mui/icons-material/Pause';
-import ReplayIcon from '@mui/icons-material/Replay';
+import ReactPlayer from "react-player";
+import PlayArrowIcon from "@mui/icons-material/PlayArrow";
+import PauseIcon from "@mui/icons-material/Pause";
+import ReplayIcon from "@mui/icons-material/Replay";
 import styles from "../styles/ShortsShare.module.css";
 
 function ShortsShare() {
-    const [isPlaying, setIsPlaying] = useState(true);
-    const [ended, setEnded] = useState(false);
-    const [played, setPlayed] = useState(0);
-    const [duration, setDuration] = useState(0);
-    const playerRef = useRef(null);
-    const location = useLocation();
-    const [id, setId] = useState("");
-    const [shortform, setShortform] = useState([]);
-    const [widthSize, setWidthSize] = useState(window.innerWidth);
-    const [heightSize, setHeightSize] = useState(window.innerHeight);
+  const [isPlaying, setIsPlaying] = useState(true);
+  const [ended, setEnded] = useState(false);
+  const [played, setPlayed] = useState(0);
+  const [duration, setDuration] = useState(0);
+  const playerRef = useRef(null);
+  const location = useLocation();
+  const [id, setId] = useState("");
+  const [shortform, setShortform] = useState([]);
+  const [widthSize, setWidthSize] = useState(window.innerWidth);
+  const [heightSize, setHeightSize] = useState(window.innerHeight);
 
-    // const handlePlayPause = () => {
-    //     if (ended) {
-    //       playerRef.current.seekTo(0);
-    //       setEnded(false);
-    //     }
-    //     setIsPlaying(!isPlaying);
-    //   };
+  // const handlePlayPause = () => {
+  //     if (ended) {
+  //       playerRef.current.seekTo(0);
+  //       setEnded(false);
+  //     }
+  //     setIsPlaying(!isPlaying);
+  //   };
 
-      const handleVideoEnded = () => {
-        setIsPlaying(false);
-        setEnded(true);
-      };
+  const handleVideoEnded = () => {
+    setIsPlaying(false);
+    setEnded(true);
+  };
 
-    useEffect(() => {
-        const searchParams = new URLSearchParams(location.search);
-        const idValue = searchParams.get("id");
-        setId(idValue);
-        
-    }, [location]);
+  useEffect(() => {
+    const searchParams = new URLSearchParams(location.search);
+    const idValue = searchParams.get("id");
+    setId(idValue);
+  }, [location]);
 
-    useEffect(()=>{
-        const fetchShortformData = async()=> {
-            try {
-                if(id){
-                const data = await getShortformDetail(id);
-                console.log(data.data);
-                setShortform(data.data);
-                }
-            } catch (error){
-                console.error('Error fetching shortform data:', error);
-            }
+  useEffect(() => {
+    const fetchShortformData = async () => {
+      try {
+        if (id) {
+          const data = await getShortformDetail(id);
+          console.log(data.data);
+          setShortform(data.data);
         }
-        fetchShortformData();
-    },[id]);
+      } catch (error) {
+        console.error("Error fetching shortform data:", error);
+      }
+    };
+    fetchShortformData();
+  }, [id]);
 
-    return (
-        <div>
-            <div style={{color:"white"}}>결과</div>
-            <ReactPlayer
-          url={shortform.video_url}
-          ref={playerRef}
-          playing={isPlaying}
-          width={widthSize}
-          height={heightSize * 0.75}
-          onDuration={setDuration}
-          loop={true}
+  return (
+    <div style={{ position: "relative", zIndex: 10 }}>
+      <div style={{ color: "white" }}>결과</div>
+      <ReactPlayer
+        url={shortform.video_url}
+        ref={playerRef}
+        playing={isPlaying}
+        width={widthSize}
+        height={heightSize * 0.75}
+        onDuration={setDuration}
+        loop={true}
         //   playbackRate={playbackRate}
-          onEnded={handleVideoEnded}
-          onProgress={({ played, loadedSeconds }) => {
-            setPlayed(played);
-            setDuration(loadedSeconds);
-          }}
+        onEnded={handleVideoEnded}
+        onProgress={({ played, loadedSeconds }) => {
+          setPlayed(played);
+          setDuration(loadedSeconds);
+        }}
         //   volume={volume}
-          playsinline
-        />
-        {/* <div className={styles.playButton} onClick={handlePlayPause}>
+        playsinline
+      />
+      {/* <div className={styles.playButton} onClick={handlePlayPause}>
             {ended ? <ReplayIcon fontSize="large" /> : isPlaying ? <PauseIcon fontSize="large" /> : <PlayArrowIcon fontSize="large" />}
         </div> */}
-        </div>
-    );
-  }
-  
-  export default ShortsShare;
-  
+    </div>
+  );
+}
+
+export default ShortsShare;
