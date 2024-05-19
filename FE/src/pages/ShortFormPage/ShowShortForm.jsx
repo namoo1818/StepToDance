271c04dcdc67a2a6dbfaf985ada72b6c7a5b60d2
3dlalswl3 <3dlalswl3@naver.com>
2024-05-19 20:01:29 +0900

    fix: 숏폼 페이지 라우팅 문제 수정
    Former-commit-id: b4efbabfbf94dba9a58cc2646bcf377aa1e02439

--- FE/src/pages/ShortFormPage/ShowShortForm.jsx
+++ FE/src/pages/ShortFormPage/ShowShortForm.jsx
@@ -113,17 +113,21 @@ useEffect(() => {
     startY = pageY;
   };  
 
-  // 스크롤 이벤트 리스너 등록
-  outerDivRef.current.addEventListener("wheel", scrollHandler, { passive: false });
+  if (outerDivRef.current) {
+    // 스크롤 이벤트 리스너 등록
+    outerDivRef.current.addEventListener("wheel", scrollHandler, { passive: false });
 
-  // 터치 이벤트 리스너 등록
-  outerDivRef.current.addEventListener("touchmove", touchMoveHandler, { passive: false });
+    // 터치 이벤트 리스너 등록
+    outerDivRef.current.addEventListener("touchmove", touchMoveHandler, { passive: false });
 
   // 컴포넌트 언마운트 시 이벤트 리스너 제거
   return () => {
-    outerDivRef.current.removeEventListener("wheel", scrollHandler);
-    outerDivRef.current.removeEventListener("touchmove", touchMoveHandler);
+    if (outerDivRef.current) {
+      outerDivRef.current.removeEventListener("wheel", scrollHandler);
+      outerDivRef.current.removeEventListener("touchmove", touchMoveHandler);
+    }
   };
+}
 }, []);
 
 
