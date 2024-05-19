d6a73735d884b4e40a086338567ccbfeff266c74
3dlalswl3 <3dlalswl3@naver.com>
2024-05-20 05:16:52 +0900

    fix: ShootingStar 주석 해제
    Former-commit-id: 20d72ffbd199e090a7a33977f1101cbacebce1ff

--- FE/src/components/Layout.jsx
+++ FE/src/components/Layout.jsx
@@ -8,7 +8,7 @@ import ShootingStar from "./ShootingStar";
 const Layout = () => {
   return (
     <section className={styles.layout}>
-      {/* <ShootingStar /> */}
+      <ShootingStar />
       <div className={styles.header}>
         <img src={logo} className={styles.logoimg} />
         <div className={styles.sidebar}>
