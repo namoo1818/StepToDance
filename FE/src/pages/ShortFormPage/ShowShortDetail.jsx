b0ac24f185990579e068ba311a0f5ed50eaba60c
3dlalswl3 <3dlalswl3@naver.com>
2024-05-20 03:50:30 +0900

    fix: 숏폼 공유 페이지 ReactPlayer로 변경
    Former-commit-id: a35ee4354f5109e358d01fbd90b6a15f6ef35bb4

--- FE/src/pages/ShortFormPage/ShowShortDetail.jsx
+++ FE/src/pages/ShortFormPage/ShowShortDetail.jsx
@@ -1,13 +1,22 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import styles from "./ShowShortForm.module.css";
 import ShareModal from "./ShareModal";
 import { getShortformDetail } from "../../api/ShortformApis";
 import FLY from "../../assets/images/fly.png"; // <a href="https://www.flaticon.com/kr/free-icons/-" title="종이 접기 아이콘">종이 접기 아이콘 제작자: Smashicons - Flaticon</a>
+import ReactPlayer from "react-player";
+import PlayArrowIcon from "@mui/icons-material/PlayArrow";
+import PauseIcon from "@mui/icons-material/Pause";
 
 const ShowShortDetail = () => {
   const [showShortForm, setShowShortForm] = useState();
   const [renderVideo, setRenderVideo] = useState([]);
   const [isModal, setIsModal] = useState(false);
+  const [played, setPlayed] = useState(0);
+  const playerRef = useRef(null);
+  const [duration, setDuration] = useState(0);
+  const [isPlaying, setIsPlaying] = useState(true);
+  const [widthSize, setWidthSize] = useState(window.innerWidth);
+  const [heightSize, setHeightSize] = useState(window.innerHeight);
 
   useEffect(() => {
     const shortformId =
@@ -19,6 +28,10 @@ const ShowShortDetail = () => {
     getData();
   }, []);
 
+  const handlePlayPause = () => {
+    setIsPlaying(!isPlaying);
+  };
+
   useEffect(() => {
     if (showShortForm) {
       const videoList = () => {
@@ -27,12 +40,20 @@ const ShowShortDetail = () => {
             {isModal ? (
               <ShareModal infos={showShortForm} setIsModal={setIsModal} />
             ) : null}
-            <video
-              className={styles["short-video"]}
-              src={showShortForm.video_url}
-              loop
-              muted
-              playsInline></video>
+            <ReactPlayer
+              url={showShortForm.video_url}
+              ref={playerRef}
+              playing={isPlaying}
+              width={widthSize}
+              height={heightSize * 0.75}
+              onDuration={setDuration}
+              loop={true}
+              onProgress={({ played, loadedSeconds }) => {
+                setPlayed(played);
+                setDuration(loadedSeconds);
+              }}
+              playsinline
+              />
             <article className={styles["short-title"]}>
               <p>@{showShortForm.uploader}</p>
               <p>{showShortForm.song_title} - {showShortForm.singer}</p>
@@ -43,6 +64,13 @@ const ShowShortDetail = () => {
               src={FLY}
               alt=""
             />
+            {/* <div className={styles.playButton} onClick={handlePlayPause}>
+            {isPlaying ? (
+              <PauseIcon fontSize="large" />
+            ) : (
+              <PlayArrowIcon fontSize="large" />
+            )}
+          </div> */}
           </article>
         );
       };
