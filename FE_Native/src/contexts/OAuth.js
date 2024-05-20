4576ea380cce8a0e7e141420b379c32d99e4aff4
백지윤 <wendy6992@naver.com>
2024-05-20 09:49:04 +0900

    DOCS: exec 파일 추가
    Former-commit-id: 2402187c3c224409029118166d5aba1afa230217

--- FE_Native/src/contexts/OAuth.js
+++
@@ -1,6 +0,0 @@
-const KAKAO_KEY="c5b3c8b5020db7071056e75fb6aaaad7";
-
-const KAKAO_REDIRECT_URL="https://auth.expo.io/FE/api/v1/auth/login";
-
-
-export const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${KAKAO_KEY}&redirect_uri=${KAKAO_REDIRECT_URL}&response_type=code`
